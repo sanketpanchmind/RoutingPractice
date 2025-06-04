770e90edd04eb954ca77fa69a51de8a4a25a5481
Sanket <panchmind1@gmail.com>
2025-06-04 18:36:11 +0530

    feat: add ngx-mat-select-search for enhanced organization selection in leave type registration

--- src/app/secure/securepages/hrms/leavetype-register/leavetype-register.component.ts
+++ src/app/secure/securepages/hrms/leavetype-register/leavetype-register.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-leavetype-register',
@@ -6,5 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./leavetype-register.component.scss']
 })
 export class LeavetypeRegisterComponent {
+  selectControl = new FormControl();
+  searchControl = new FormControl();
+  searchSubject: ReplaySubject<any> = new ReplaySubject<any>();
+  options: any[] = [];
+  filteredOptions: any[] = [];
+
+  organizationArray: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.getorglist();
+    this.onSearch();
+
+
+  }
+
+  onSearch() {
+    this.searchControl.valueChanges.pipe().subscribe(() => { this.filterArrayDataZone(this.organizationArray, this.searchControl, 'text', this.searchSubject) });
+  }
+
+
+  getorglist() {
+    this.http
+      .get('https://demo-api-hrms.hitechdairy.in/api/Dropdown/get-organization-list?appId=1&organizationId=0&isExternal=true')
+      .subscribe({
+        next: (res: any) => {
+          console.log("org--", res.responseData);
+
+          this.organizationArray = res.responseData;
+          this.filterArrayDataZone(this.organizationArray, this.searchControl, 'text', this.searchSubject);
+        },
+        error: (error: any) => {
+          console.log("org err--", error);
+        }
+      });
+  }
+
+
+  filterArrayDataZone(array: any, formControl: any, keyName: any, subjectName: any) {
+    if (!array) { return; }
+    let search = formControl.value;
+    if (!search) {
+      subjectName.next(array.slice());
+      return;
+    } else { search = search.toLowerCase(); }
+
+    subjectName.next(array.filter( // filter the array data
+      (ele: any) => ele[keyName]?.toLowerCase().indexOf(search) > -1));
+  }
 
 }
--- src/app/secure/securepages/hrms/leavetype-register/leavetype-register.module.ts
+++ src/app/secure/securepages/hrms/leavetype-register/leavetype-register.module.ts
@@ -3,6 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { LeavetypeRegisterRoutingModule } from './leavetype-register-routing.module';
 import { LeavetypeRegisterComponent } from './leavetype-register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 
 
 @NgModule({
@@ -11,7 +28,25 @@ import { LeavetypeRegisterComponent } from './leavetype-register.component';
   ],
   imports: [
     CommonModule,
-    LeavetypeRegisterRoutingModule
+    LeavetypeRegisterRoutingModule,
+    MatCardModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatIconModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatDialogModule,
+        MatTooltipModule,
+        MatRadioModule,
+        HttpClientModule,
+        MatTableModule,
+        MatAutocompleteModule,
+        MatSnackBarModule,
+        MatDatepickerModule,MatNativeDateModule,
+        NgxMatSelectSearchModule
+        
   ]
 })
 export class LeavetypeRegisterModule { }
